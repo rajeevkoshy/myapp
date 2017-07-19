0276ffdf47daea21e30f31d3da90f62ef5384cfb
rajeevkoshy <rkoshy@mafcarrefour.com>
2017-07-19 12:29:28 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var path = require('path');
+ var express = require('express');
+var path = require('path'); //git conflict test
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
